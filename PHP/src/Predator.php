3f795e7345ffa48a8d5c320102dda6d76dcaa311
IzaMydlo <izabemudlo@gmail.com>
2023-03-03 22:22:39 +0100

    Cleaning before main app implementation

--- PHP/src/Predator.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace src;
-
-interface Predator
-{
-    public function chase(Prey $prey); //ensuring that only Pray are chased
-    public function kill(Prey $prey);
-}
--- oop/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once 'parseform.php';
-
-require_once 'form.php';
-
-
